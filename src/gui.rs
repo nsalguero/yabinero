9b3932d0ae4fc0ff05752be510e1fec3127279fb
Nicolas SALGUERO <nicolas.salguero@laposte.net>
2020-05-18 16:42:27 +0200

    Tests with FLTK

---
+++ src/gui.rs
@@ -0,0 +1,6 @@
+use fltk::{app::*, button::*, frame::*, window::*};
+use std::fs::File;
+use std::io::BufReader;
+use rodio::Source;
+
+
--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,13 @@
 //}
 
 use fltk::{app::*, button::*, frame::*, window::*};
+use std::time::{Duration, Instant};
+use std::thread;
+use std::sync::mpsc;
 use std::fs::File;
 use std::io::BufReader;
 use rodio::Source;
+use std::path::Path;
 
 fn main() {
     let app = App::default();
@@ -27,9 +31,28 @@ fn main() {
     let mut but = Button::new(160, 210, 80, 40, "Click me!");
     wind.end();
     wind.show();
-    but.set_callback(Box::new(|| {
+    let now = Instant::now();
+    const WAITING_DURATION: Duration = Duration::from_millis(100);
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let mut pause = false;
+        loop {
+            thread::sleep(WAITING_DURATION);
+            if let Ok(p) = rx.try_recv() {
+                pause = p;
+            }
+            if !pause {
+                let duration = now.elapsed().as_secs();
+                let duration_as_str = format!("{:02}:{:02}:{:02}", duration / 3600, duration / 60, duration % 60);
+                frame.set_label(&duration_as_str);
+            }
+        }
+    });
+    but.set_callback(Box::new(move || {
+        app.set_scheme(AppScheme::Gtk);
+        tx.send(true).unwrap();
         let device = rodio::default_output_device().unwrap();
-        let file = File::open("sounds/success.ogg").unwrap();
+        let file = File::open(Path::new("sounds").join("success.ogg")).unwrap();
         let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
         rodio::play_raw(&device, source.convert_samples());
     }));
